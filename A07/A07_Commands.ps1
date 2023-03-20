2e19c231779804bb4191df1a008429b8725177fd
Tom Wechsler <tom@tomsazure.ch>
2023-03-20 13:32:34 +0100

    new files

---
+++ A07/A07_Commands.ps1
@@ -0,0 +1,5 @@
+#Create a Terraform execution plan
+terraform plan -out main.tfplan
+
+#Apply a Terraform execution plan
+terraform apply main.tfplan
---
+++ A07/README.md
@@ -0,0 +1,1 @@
+**# Create network interface in azure**
