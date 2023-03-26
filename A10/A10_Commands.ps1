b1fdfa5e19f7a38db97cbf1ad7df34df47be5401
Tom Wechsler <tom.wechsler@outlook.com>
2023-03-26 14:56:34 +0200

    lines edited

--- A10/A10_Commands.ps1
+++ A10/A10_Commands.ps1
@@ -1,6 +1,9 @@
 #Validate the configuration file
 terraform validate
 
+#Rewrites config files to canonical format
+terraform fmt
+
 #Create a Terraform execution plan
 terraform plan -out main.tfplan
 
