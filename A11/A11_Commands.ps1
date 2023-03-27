f9d9eb203cc3fd8dad7a3ccbb24cbc20382c6cf6
Tom Wechsler <tom.wechsler@outlook.com>
2023-03-27 06:46:54 +0200

    newfiles

---
+++ A11/A11_Commands.ps1
@@ -0,0 +1,14 @@
+#Validate the configuration file
+terraform validate
+
+#Rewrites config files to canonical format
+terraform fmt
+
+#Create a Terraform execution plan
+terraform plan -out main.tfplan
+
+#Apply a Terraform execution plan
+terraform apply main.tfplan
+
+#Be careful with this info
+terraform output -raw tls_private_key > id_rsa
---
+++ A11/README.md
@@ -0,0 +1,1 @@
+**Create (and display) an SSH key**
